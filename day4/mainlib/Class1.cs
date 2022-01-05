7d526c2f35f42ef9f40e1d549f3c62f60c8ae96d
Alex <gurkslask@gmail.com>
2022-01-05 11:59:44 +0100

    Day 4 basic is done

--- day4/mainlib/Class1.cs
+++ day4/mainlib/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 
 namespace mainlib
@@ -21,34 +22,59 @@ namespace mainlib
             return s;
         }
         public static int SolveBasic(string ss){
+
             string random_numbers = ss.Split('\n')[0];
             System.Console.WriteLine($"Random numbers är: {random_numbers}");
 
             List<board> lGrid = new List<board>();
+
             string[] s = ss.Split('\n');
-            // Make grids
-            for (int i = 2; i < (s.Count() -2) % 6; i+= 6)
+            float modulusen = (s.Count() - 1) / 6;
+            int lower = 2;
+            int upper = 7;
+            for (int i = 0; i < modulusen; i += 1)
             {
-                int k = i + 6;
-                System.Console.WriteLine($"Här kommer lite data {string.Join(' ', s[i..k])}");
-                lGrid.Add(new board(string.Join('\n', s[i..k])));
+                lGrid.Add(new board(string.Join('\n', s[lower..upper])));
+                lower += 6;
+                upper += 6;
+            }
+            int c = 1;
+            Console.WriteLine(lGrid.Count());
+            foreach (board b in lGrid)
+            {
+                Console.WriteLine("Init: \n " + b.ToString());
             }
             int res = 0;
+            bool done = false;
             // Loop through numbers and check if done
-            foreach (string number in random_numbers.Split(','))
+            foreach (string snumber in random_numbers.Split(','))
             {
+                int number = int.Parse(snumber);
                 foreach (board g in lGrid)
                 {
-                    g.insertnum(int.Parse(number));
+                    g.insertnum((number));
                     if (g.checkIfBoardIsWin()) {
-                        res = g.countNonMarked() * res;
-                    }
+                        Console.WriteLine("********** KLART ********");
+                        foreach(board b in lGrid){
+                            res = g.countNonMarked() * number;
+                            done = true;
+                            Console.WriteLine(res);
+                        }
+                    g.Print();
                     
+                    }
                 }
+                if (done) { break;}
             }
-            return res;
+        Console.WriteLine(lGrid.Count());
+        foreach (board b in lGrid)
+        {
+            Console.WriteLine(b.ToString());
         }
-        public static int SolveAdv(string s){
+        return res;
+    }
+
+    public static int SolveAdv(string s){
             return 42;
         }
     public static List<string> removeFromList(List<string> inlist, string siffra, int rempovepos){
@@ -85,21 +111,39 @@ namespace mainlib
                 this.check = check;
             }
         }
-        gridItem[,] grid = new gridItem[4,4];
+        gridItem[,] grid = new gridItem[5,5];
+        public override string ToString()
+        {
+            string res = "";
+            int c = 0;
+            foreach (gridItem row in grid)
+            {
+                res += $"S: {row.num}, c: {row.check};";
+                c += 1;
+                if (c == 5){
+                    res += "\n";
+                    c = 0;
+                }
+            }
+            return res;
+
+        }
         public board(string boardInput)
         {
             int irow = 0;
             int icol = 0;
             foreach (string row in boardInput.Split("\n"))
             {
-                foreach (string col in row.Split(" "))
+                foreach (string col in Regex.Split(row, @"\s+"))
                 {
-                    if (col != "  "){
-                        System.Console.WriteLine($"co: {col}, len {col.Length}");
+                    if (col.Length > 0){
+                        // System.Console.WriteLine($"co: {col}, len {col.Length}");
+                        // System.Console.WriteLine($"icol {icol}, irow{irow}");
                         grid[irow, icol] = new gridItem(int.Parse(col), false);
                         icol += 1;
                     }
                 }
+                icol = 0;
                 irow += 1;
             }
         }
@@ -108,10 +152,10 @@ namespace mainlib
             bool done = false;
             for (int i = 0; i < 4; i++)
             {
-                if (grid[i, 0].check && grid[i, 1].check && grid[i, 2].check && grid[i, 3].check && grid[i, 4].check  ) {
+                if (grid[i, 0].check && grid[i, 1].check && grid[i, 2].check && grid[i, 3].check && grid[i, 4].check  && grid[i, 4].check  ) {
                     done = true;
                 }
-                if (grid[0, i].check && grid[1, i].check && grid[2, i].check && grid[3, i].check && grid[4, i].check  ) {
+                if (grid[0, i].check && grid[1, i].check && grid[2, i].check && grid[3, i].check && grid[4, i].check  && grid[4, i].check  ) {
                     done = true;
                 }
             }
@@ -121,30 +165,40 @@ namespace mainlib
         public int countNonMarked()
         {
             int count = 0;
-            for (int row = 0; row < 4; row++)
+            for (int row = 0; row < 5; row++)
             {
-                for (int col = 0; col < 4; col++)
+                for (int col = 0; col < 5; col++)
                 {
                     if(!grid[row, col].check){
                         count += grid[row, col].num;
+                        Console.WriteLine($"{grid[row, col].num}");
                     }
                 }
             }
             return count;
         }
-        public void insertnum(int inNum){
-            for (int row = 0; row < 4; row++)
+        public void Print(){
+            for (int row = 0; row < 5; row++)
             {
-                for (int col = 0; col < 4; col++)
+                for (int col = 0; col < 5; col++)
+                {
+                    if(!grid[row, col].check){
+                        //System.Console.WriteLine($"{grid[row, 0].num} : {grid[row, 0].check} , {grid[row, 1].num} : {grid[row, 1].check} , {grid[row, 2].num} : {grid[row, 2].check} ");
+                    }
+                }
+            }
+        }
+        public void insertnum(int inNum){
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
                 {
                     if(grid[row, col].num == inNum){
                         grid[row, col].check = true;
                     }
                 }
             }
-
         }
-
         }
     }
 
